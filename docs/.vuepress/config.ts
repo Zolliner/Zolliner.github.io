859e0ce127abe812991c7fee97e1ae460539513f
lizhendong128 <lizhendong128@126.com>
2022-09-25 18:59:53 +0800

    修改

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -13,9 +13,9 @@ export default defineUserConfig({
   //网站语言，默认为中文
   lang: "zh-CN",
   //网站标题
-  title: "LearnData-开源笔记",
+  title: "李振东的知识库",
   //网站描述
-  description: "开源工具、效率方法、心理学探索的自我提升笔记，记录并输出一切能让自己提升的知识。",
+  description: "记录生活，分享知识，留住记忆。",
 
   //网站路径，默认为 /，如果非主域名，需改为
   base: "/",
@@ -28,12 +28,6 @@ export default defineUserConfig({
   templateBuild: path.resolve(__dirname, "templateBuild.html"),
 
   plugins: [
-    //algolia 全文搜索：没设置爬虫的话，需删除 docsearchPlugin 区块以使用节点搜索
-    docsearchPlugin({
-      indexName: 'newzone',
-      appId: 'M4EXXEZIEG',
-      apiKey: 'fd8891a9c4cc21e0ef4f11bf44f7a11e',
-    }),
     //本地搜索：默认情况下，该插件会将页面标题和小标题作为搜索索引。
     searchPlugin({
       // 你的选项
